9d97a4dbb646a424a5f5aaa71052e7bebaf26187
Prince Kumar <ajha4734@gmail.com>
2025-12-03 00:51:40 +0530

    Create abstract Person class with name and email

---
+++ Person.java
@@ -0,0 +1,15 @@
+package model;
+
+public abstract class Person {
+    protected String name;
+    protected String email;
+
+    public Person() {}
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public abstract void displayInfo();
+}
